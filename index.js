4bd50d518882f78ebcf03ccf97d7c9980b78398b
atikin <nikvmain@gmail.com>
2024-03-01 11:54:28 +0300

    Change order

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const callbacks = require("./callbacks");
-const paralleljs = require("./parallel");
 const promises = require("./promises");
+const paralleljs = require("./parallel");
 
 const main = async () => {
   console.log("Callbacks:");
   callbacks();
 
-  console.log("paralleljs:");
-  await paralleljs();
-
   console.log("Promises:");
   await promises();
+
+  console.log("paralleljs:");
+  await paralleljs();
 };
 
 main();
